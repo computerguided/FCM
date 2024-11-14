fad0b5abf0df49a80dd717eb603eeda5f6e5dbdd
Fred Dijkstra <fred@computerguided.com>
2024-11-14 13:27:38 +0100

    Worker Handler added

---
+++ inc/FcmWorkerHandler.h
@@ -0,0 +1,48 @@
+#ifndef FCM_WORKER_H
+#define FCM_WORKER_H
+
+#include <string>
+#include <atomic>
+#include <thread>
+
+#include "FcmBaseComponent.h"
+
+// ---------------------------------------------------------------------------------------------------------------------
+class FcmWorkerHandler: public FcmBaseComponent
+{
+public:
+    explicit FcmWorkerHandler(const std::string& nameParam,
+                       const FcmSettings& settingsParam = {});
+
+    void initialize() override;
+
+    // Starts the worker thread
+    bool start();
+
+    // Cancels the worker thread
+    void cancel();
+
+    ~FcmWorkerHandler() = default;
+
+protected:
+    // Method to be overridden by subclass to perform the long-running task
+    virtual void run() = 0;
+
+    // Method to be overridden by subclass to handle task completion
+    virtual std::shared_ptr<FcmMessage> prepareFinishedMessage() = 0;
+    virtual void finished() = 0;
+
+private:
+    std::thread workerThread; // Empty thread object (not yet running)
+    std::atomic<bool> cancelRequested{false};
+
+    // Internal thread function that runs the worker and calls finished()
+    void threadRun();
+
+    // Placeholder for the finished message
+    std::shared_ptr<FcmMessage> finishedMessage;
+
+};
+
+#endif //FCM_WORKER_H
+
---
+++ src/FcmWorkerHandler.cpp
@@ -0,0 +1,67 @@
+#include "FcmWorkerHandler.h"
+
+// ---------------------------------------------------------------------------------------------------------------------
+FcmWorkerHandler::FcmWorkerHandler(const std::string& nameParam,
+                                   const FcmSettings& settingsParam):
+    FcmBaseComponent(nameParam, settingsParam)
+{
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+FcmWorkerHandler::~FcmWorkerHandler()
+{
+    cancel();
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+bool FcmWorkerHandler::start()
+{
+    if (workerThread.joinable())
+    {
+        logError("Worker: " + name + " already started");
+        return false;
+    }
+
+    cancelRequested = false;
+    workerThread = std::thread(&FcmWorkerHandler::threadRun, this);
+    return true;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+void FcmWorkerHandler::cancel()
+{
+    cancelRequested = true;
+
+    if (workerThread.joinable())
+    {
+        // Block the main thread until the worker thread has handled the stop request in the run() method.
+        workerThread.join();
+    }
+
+    // Remove the possible finished message from the message queue
+    auto checkFunction = [this](const std::shared_ptr<FcmMessage>& msg) -> bool
+    {
+        return msg->sender == this;
+    };
+
+    messageQueue.removeMessage(finishedMessage->_interfaceName,
+                               finishedMessage->_name,
+                               checkFunction);
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+void FcmWorkerHandler::threadRun()
+{
+    run();
+
+    if (cancelRequested)
+    {
+        return;
+    }
+
+    finished();
+
+    // Send the finished message.
+    finishedMessage = prepareFinishedMessage();
+    sendMessage(finishedMessage);
+}
