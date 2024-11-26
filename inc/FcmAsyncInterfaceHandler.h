45529deca61586196fd41b3069f3fee9ac06b0e1
Fred Dijkstra <fred@computerguided.com>
2024-11-26 09:26:40 +0100

    Update FcmAsyncInterfaceHandler.h

--- inc/FcmAsyncInterfaceHandler.h
+++ inc/FcmAsyncInterfaceHandler.h
@@ -1,20 +1,15 @@
 #ifndef FCM_ASYNC_INTERFACE_HANDLER_H
 #define FCM_ASYNC_INTERFACE_HANDLER_H
 
-#include <string>
-
 #include "FcmBaseComponent.h"
-#include "FcmMessage.h"
-#include "FcmMessageQueue.h"
 
 // ---------------------------------------------------------------------------------------------------------------------
 class FcmAsyncInterfaceHandler: public FcmBaseComponent
 {
 public:
     using FcmBaseComponent::FcmBaseComponent;
-    virtual void initialize() override {}; // Override in derived classes if needed.
+    virtual void initialize() = 0; // Override in derived classes if needed.
 };
-    
 
 // ---------------------------------------------------------------------------------------------------------------------
 #define FCM_ASYNC_INTERFACE_HANDLER(NAME, ...) \
